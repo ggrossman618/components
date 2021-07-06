87a8afbd1345a252eb41de2bc27025649cd09869
ggrossman618 <ggrossman@colgate.edu>
2021-07-06 15:17:35 -0400

    added server-side compatibility (untested), created productData interface

--- packages/components/src/table/MyTable.tsx
+++ packages/components/src/table/MyTable.tsx
@@ -1,11 +1,66 @@
 import * as React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
+import { useDemoData } from '@material-ui/x-grid-data-generator';
 
+
+interface productData {
+  _id: string;
+  product: string;
+  name: string;
+  version: string;
+  maintainer: string;
+  fileWebsite: string;
+  __v: number;
+  fileElf: string;
+
+}
+
+function loadServerRows(page, data) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(data.rows.slice(page * 5, (page + 1) * 5));
+    }, Math.random() * 500 + 100); // simulate network latency
+  });
+}
 
 export function MyTable(props) {
+
+  const { data } = useDemoData({
+    dataSet: 'Commodity',
+    rowLength: 100,
+    maxColumns: 6,
+  });
+
+  const [page, setPage] = React.useState(0);
+  const [rows, setRows] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
+  const handlePageChange = (params) => {
+    setPage(params.page);
+  };
+
+  React.useEffect(() => {
+    let active = true;
+
+    (async () => {
+      setLoading(true);
+      const newRows = await loadServerRows(page, data);
+
+      if (!active) {
+        return;
+      }
+
+      setRows(newRows);
+      setLoading(false);
+    })();
+
+    return () => {
+      active = false;
+    };
+  }, [page, data]);
+
   const columns = [
-    { field: 'id', headerName: 'Index', width: 40 },
-    { field: 'productId', headerName: 'ID', width: 200 },
+    { field: 'id', headerName: 'ID', width: 200 },
     { field: 'product', headerName: 'Product Name', width: 225 },
     { field: 'version', headerName: 'Version', width: 125 },
     { field: 'maintainer', headerName: 'Maintainer', width: 150 },
@@ -14,9 +69,8 @@ export function MyTable(props) {
     { field: 'v', headerName: '__v', width: 100 },
   ];
 
-  const products = props.productsData.map((d, index) => ({
-    id: index + 1,
-    productId: d._id,
+  const products: productData = props.productsData.map((d) => ({
+    id: d._id,
     product: d.name,
     version: d.version,
     maintainer: d.maintainer,
@@ -26,8 +80,6 @@ export function MyTable(props) {
     
   }));
 
-  const rows = [{ id: 0, product: 'test' }];
-
   return (
     <div style={{ height: 400, width: '100%' }}>
       <DataGrid
@@ -36,6 +88,9 @@ export function MyTable(props) {
         pageSize={15}
         density="compact"
         autoHeight={true}
+        paginationMode="server"
+        onPageChange={handlePageChange}
+        loading={loading}
       />
       
     </div>
